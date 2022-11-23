38408d3c738a01274ac4725a4f678ac50ca49234
Dan Mopsick <danmopsick@gmail.com>
2022-11-23 08:13:32 -0500

    #1 created Client file that the server file will use to represent a client in the system

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 
  # The server itself that will run the Pokémon battle simulator
 class Server(object):
-   
 
     def __init__(self):
         # Build and start the server
 
         # Init some variables
+        self.clientList = {}
+        self.nextId = 0 # Next Id to assign to client as they connect
 
+        # Create socket to listen for clients
+        self
 
 
         pass
