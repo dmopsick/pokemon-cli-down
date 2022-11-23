9f97a33656ecdc35e1b28222c060e38533261080
Dan Mopsick <danmopsick@gmail.com>
2022-11-23 08:42:39 -0500

    #1 creating an engine file to run the actual game loop and use the server file built

---
+++ engine.py
@@ -0,0 +1,13 @@
+# This is the game engine that runs the game
+# Contains the main loop and state based logic
+
+from server import Server
+
+# Start the server
+server = Server()
+
+while True:
+    # Perhaps pause the loop 
+
+    s
+
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import socket
 
  # The server itself that will run the Pokémon battle simulator
 class Server(object):
@@ -10,7 +11,26 @@ class Server(object):
         self.nextId = 0 # Next Id to assign to client as they connect
 
         # Create socket to listen for clients
-        self
+        self.listeningSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        # Set option on the socket
+        self.listeningSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+        # Define a address and port to listen on
+        # Telnet standard port 23, this project using different port to avoid potential permission issues
+        self.listeningSocket.bind(("0.0.0.0", 1234))
+        
+        # Set to non blocking mode to not wait for connection
+        self.listeningSocket.setblocking(False)
+
+        # Listen for connections
+        self.listeningSocket.listen(1)
+
+        # Set the server state
+
+        print ("CLIDown server started...")
+
+    def update(self):
         pass
+
+    
