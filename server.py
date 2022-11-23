c9e07e04d38058fb84ad89fc763340dca67fb074
Dan Mopsick <danmopsick@gmail.com>
2022-11-23 07:51:56 -0500

    #1 creating the server file

---
+++ server.py
@@ -0,0 +1,13 @@
+
+ # The server itself that will run the Pokémon battle simulator
+class Server(object):
+   
+
+    def __init__(self):
+        # Build and start the server
+
+        # Init some variables
+
+
+
+        pass
