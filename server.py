8bd87b1e761329e7cb8dc1238662d3243f194c85
Dan Mopsick <danmopsick@gmail.com>
2022-11-27 10:14:51 -0500

    #2 wrote first iteration of the code to load a new client

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import socket
 from utils import ServerStates
 import utils
+import select
+import client
+import time
 
  # The server itself that will run the Pokémon battle simulator
 class Server(object):
@@ -11,7 +14,7 @@ class Server(object):
 
         # Init some variables
         self.clientList = {}
-        self.nextId = 0 # Next Id to assign to client as they connect
+        self.nextClientId = 0 # Next Id to assign to client as they connect
 
         # Create socket to listen for clients
         self.listeningSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,6 +38,42 @@ class Server(object):
         print ("CLIDown server started...")
 
     def update(self):
+        pass
+
+    # Check for a new client connecting to the game server
+    def checkNewConnections(self):
+        # Check 3 lists of sockets, only concerned about the first one, the reading sockets
+        rlist, wlist, xlist = select.select([self.listeningSocket], [], [], 0)
+        
+        # Check if the listening socket is in the readable list
+        if self.listeningSocket in rlist:
+            # The defined listening socket contains data to be read
+            # Accept the new socket
+            acceptedSocket, addr = self.listeningSocket.accept()
+
+            # Set non-blocking mode on the socket so the send and recv will occur instantly
+            acceptedSocket.setblocking(False)
+
+            # Construct new client object
+            client = client.Client(acceptedSocket, addr, '', time.time())
+
+            # Add the object to the server's list of clients
+            self.clientList[self.nextClientId] = client
+            
+            # Increment the nextClientId in preperation of the next client
+            self.nextClientId += 1
+
+            # Check to see if there are now two clients
+            if len(self.clientList) == 2:
+                # If so the connections have been established and the battle can begin
+                self.state = utils.ServerStates.ESTAB
+            elif len(self.clientList) > 2:
+                print("WARNING: There are " + str(len(self.clientList)) + " connections detected.")
+        
+        else:
+            # There is not data to be read at this time at the socket we are listening on
+            print("NO DATA AVAILABLE TO READ")
+            return
         pass
 
     
--- utils.py
+++ utils.py
@@ -3,10 +3,10 @@ from enum import Enum
 # Hold the states that the engine will use to determine  
 class ServerStates(Enum):
     CLOSED, LISTEN,\
-    ESTAB = range(1, 3)
+    ESTAB, FIN = range(1, 4)
 
 class GameStates(Enum):
     BATTLE_START, ACCEPT_COMMANDS, \
     CALCULATE_RESULT, DISPLAY_RESULT, \
-    END_BATTLE = range(1,5)
+    END_BATTLE, CLOSED= range(1,6)
 
